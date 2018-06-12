29ccdac1e67d860dfab7ac13730c8361c25b697c
Kevin Huang <yellow.khaos@gmail.com>
2018-06-12 06:09:43 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,6 @@ class PostsController < ApplicationController
 	def update
 		@post = Post.find(params[:id])
 		@post.update(title: params[:title], description: params[:description])
-		redirect_to posts_path
+		redirect_to post_path(@post)
 	end
 end
